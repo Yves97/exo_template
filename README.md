fec44c67619aa8f7a629a3d37eb7410d366b2b5c
Ecole NaN <nan@iMac-23.local>
2019-11-06 16:45:44 +0000

    mise en place de la responsive navbar

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reproduction d'un template
+## lien du template: https://demo.tagdiv.com/newspaper_animals_pro/
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+(function(){
+	$(document).ready(function() {
+		//Dropdown Menu
+    $('.menu-button').on('click', function(){
+    $('nav ul').toggleClass('show');
+        });
+	});
+})(jQuery);
